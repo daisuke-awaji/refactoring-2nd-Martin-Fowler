d9d2b1addc2cbeff8021541d6c3bac718dd37036
awaji-daisuke <awaji_daisuke@intec.co.jp>
2020-04-20 12:15:04 +0900

    最低限のテスト

--- src/statement.test.ts
+++ src/statement.test.ts
@@ -1,5 +1,40 @@
 import { statement } from "./statement";
-test("a", () => {
+
+test("一定の観客数までは固定金額を請求する", () => {
+  const invoice = {
+    customer: "Walt",
+    performance: [
+      {
+        playID: "hamlet",
+        audience: 30,
+      },
+      {
+        playID: "as-like",
+        audience: 20,
+      },
+      {
+        playID: "othello",
+        audience: 10,
+      },
+    ],
+  };
+  const plays = {
+    hamlet: { name: "Hamlet", type: "tragedy" },
+    "as-like": { name: "As You Like It", type: "comedy" },
+    othello: { name: "Othello", type: "tragedy" },
+  };
+
+  const result = statement(invoice, plays);
+  expect(result).toBe(`Statement for Walt
+ Hamlet: $400.00 (30 seats)
+ As You Like It: $360.00 (20 seats)
+ Othello: $400.00 (10 seats)
+Amount owed is $1,160.00
+You earned 4 credits
+`);
+});
+
+test("観客数に応じて課金される", () => {
   const invoice = {
     customer: "BigCo",
     performance: [
