6290055d516e2fc54a5319c1813c7bb58067b66d
awaji-daisuke <awaji_daisuke@intec.co.jp>
2020-04-23 16:10:47 +0900

    勉強会その１の結果

--- src/statement.ts
+++ src/statement.ts
@@ -16,50 +16,68 @@ interface IPlays {
 }
 
 export function statement(invoice: IInvoice, plays: IPlays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
-
   for (let perf of invoice.performance) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
-
-    // ボリューム特典のポイントを加算
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 喜劇のときは 10人につき、さらにポイントを加算
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    // 注文の内訳を出力
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
-    totalAmount += thisAmount;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-
+  result += `Amount owed is ${usd(totalAmounts(invoice.performance))}\n`;
+  result += `You earned ${totalVolumeCredits(invoice.performance)} credits\n`;
   return result;
+
+  function totalAmounts(performance: IPerformance[]) {
+    let totalAmount = 0;
+    for (let perf of invoice.performance) {
+      totalAmount += amountFor(perf);
+    }
+    return totalAmount;
+  }
+
+  function usd(aNumber: number) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
+
+  function totalVolumeCredits(performance: IPerformance[]) {
+    let result = 0;
+    for (let perf of performance) {
+      result += volumeCreditsFor(perf, playFor(perf));
+    }
+    return result;
+  }
+
+  function volumeCreditsFor(perf: IPerformance, play: IPlay) {
+    let result = Math.max(perf.audience - 30, 0);
+    if ("comedy" === play.type) result += Math.floor(perf.audience / 5);
+    return result;
+  }
+  function playFor(perf: IPerformance) {
+    return plays[perf.playID];
+  }
+  function amountFor(perf: IPerformance) {
+    let result = 0;
+    switch (playFor(perf).type) {
+      case "tragedy":
+        const TRAGEDY_BASE_USD = 40000;
+        result = TRAGEDY_BASE_USD;
+        if (perf.audience > 30) {
+          result += 1000 * (perf.audience - 30);
+        }
+        return result;
+      case "comedy":
+        const COMEDY_BASE_USD = 30000;
+        result = COMEDY_BASE_USD;
+        if (perf.audience > 20) {
+          result += 10000 + 500 * (perf.audience - 20);
+        }
+        result += 300 * perf.audience;
+        return result;
+      default:
+        throw new Error(`unknown type: ${playFor(perf).type}`);
+    }
+  }
 }
